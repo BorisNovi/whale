7681ac05480db72f0bf9e52e7b00009b659e9f37
Boris Novikov <borisnovikov4815@gmail.com>
2024-11-04 12:28:21 +0400

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://0.0.0.0:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
