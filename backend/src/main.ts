b963f51c7f34ce3bb966f412df97c2566ecb6b52
Boris Novikov <borisnovikov4815@gmail.com>
2024-11-04 12:36:55 +0400

    feat: add cors for real hosting, change environments

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://0.0.0.0:4200',
+    origin: ['http://0.0.0.0:4200', 'http://5.187.0.124:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
